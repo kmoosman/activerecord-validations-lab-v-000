f5bfbff1fe0b5cd68f3c86171bf6ab1071c25b8e
Katie Moosman <moosman.katie@gmail.com>
2019-04-05 21:45:13 +0000

    Automatically backed up by Learn

--- app/helpers/my_validator.rb
+++ app/helpers/my_validator.rb
@@ -1,7 +1,8 @@
 class MyValidator < ActiveModel::Validator
   def validate(record)
     if record.title
-      if ["Won't Believe", "Secret", "Top[number]", "Guess"].include? record.title
+      clickbait = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+      if a.include?(record.title)
       # if clickbait.detect {|el| record.title.include?(el) }.nil?
       else
         record.errors[:title] << "Your title must contain clickbait" 
