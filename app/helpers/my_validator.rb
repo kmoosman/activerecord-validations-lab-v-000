d5c397c24b3a8d59ee9d3360a264de272284a711
Katie Moosman <moosman.katie@gmail.com>
2019-04-05 21:36:28 +0000

    Automatically backed up by Learn

--- app/helpers/my_validator.rb
+++ app/helpers/my_validator.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 class MyValidator < ActiveModel::Validator
   def validate(record)
-=======
-class CustomValidator < ActiveModel::Validator
-  def validate?(record)
->>>>>>> f08f2e09c0f90d08b155dbc1a39d39d0d4881e25
     if record.title
       reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
       if reqs.detect {|el| record.title.include?(el) }.nil?
@@ -12,8 +7,4 @@ class CustomValidator < ActiveModel::Validator
       end
     end
   end
-<<<<<<< HEAD
 end
-=======
-end 
->>>>>>> f08f2e09c0f90d08b155dbc1a39d39d0d4881e25
